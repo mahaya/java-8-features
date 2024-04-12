524721eb4908a948699fe362c04e53bdd2368b68
mahaya <donaldmahaya@yahoo.com>
2024-04-12 17:47:36 +0000

    Changed text of println

--- HelloCodespace.java
+++ HelloCodespace.java
@@ -1,6 +1,6 @@
 public class HelloCodespace {
 
     public static void main(String[] args) {
-        System.out.println("Hello Codespace");
+        System.out.println("Hello Codespaces");
     }
 }
