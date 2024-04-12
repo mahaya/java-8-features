93e177017b258549f0ddf913e4bc83617a6b8674
mahaya <donaldmahaya@yahoo.com>
2024-04-12 18:15:20 +0000

    student city updates

--- src/main/java/com/onlyfullstack/util/JavaInputFixture.java
+++ src/main/java/com/onlyfullstack/util/JavaInputFixture.java
@@ -9,11 +9,11 @@ public class JavaInputFixture {
 	
 	public static List<Student> createList() {
 
-		Student student1 = new Student("Saurabh", "Pune", 26, new String[] {"Java","Spring","Hibernate"});
-		Student student2 = new Student("Robert", "Pune", 25, new String[] {".Net","HTML","CSS"});
-		Student student3 = new Student("John", "Mumbai", 21, new String[] {"Typescript","Angular 4"});
-		Student student4 = new Student("Roman", "Pune", 18, new String[] {"ReactJS","JavaScript","Fidler"});
-		Student student5 = new Student("Randy", "Mumbai", 17, new String[] {"Java","Spring Cloud","Spring Data"});
+		Student student1 = new Student("Mahaya", "NY", 26, new String[] {"Java","Spring","Hibernate"});
+		Student student2 = new Student("Robert", "NY", 25, new String[] {".Net","HTML","CSS"});
+		Student student3 = new Student("John", "TX", 21, new String[] {"Typescript","Angular 4"});
+		Student student4 = new Student("Roman", "NY", 18, new String[] {"ReactJS","JavaScript","Fidler"});
+		Student student5 = new Student("Randy", "TX", 17, new String[] {"Java","Spring Cloud","Spring Data"});
 		
 		return Arrays.asList(new Student[] {student1, student2, student3, student4, student5});
 	}
